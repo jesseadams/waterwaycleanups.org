a6e47e6fdd6e3205762646b38de0d50a06539e10
Jesse Adams <jesse@techno-geeks.org>
2025-06-04 08:18:09 -0400

    Revert "Production minification (#21)"
    This reverts commit 9f8674b051a470a9391d2b12670ed8e354646d03.

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,5 @@ module.exports = {
   plugins: {
     '@tailwindcss/postcss': {},
     autoprefixer: {},
-    cssnano: {
-      preset: ['default', {
-        discardComments: {
-          removeAll: true,
-        },
-      }],
-    }
   }
 }
