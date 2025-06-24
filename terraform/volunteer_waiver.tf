443c92c565396da2774bc62bbf7305b987026d44
Jesse Adams <jesse@framework13.waterwaycleanups.org>
2025-06-24 15:58:19 -0400

    WIP

--- terraform/volunteer_waiver.tf
+++ terraform/volunteer_waiver.tf
@@ -312,7 +312,12 @@ resource "aws_api_gateway_deployment" "volunteer_waiver_deployment" {
   ]
 
   rest_api_id = aws_api_gateway_rest_api.volunteer_waiver_api.id
-  stage_name  = var.environment
+}
+
+resource "aws_api_gateway_stage" "example" {
+  deployment_id = aws_api_gateway_deployment.volunteer_waiver_deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.volunteer_waiver_api.id
+  stage_name    = var.environment
 }
 
 # Output the API URLs
